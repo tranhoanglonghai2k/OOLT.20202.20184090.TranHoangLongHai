65b4295db9e02e3a5bc93992777fac1af34c16e3
tranhoanglonghai2k <76715766+tranhoanglonghai2k@users.noreply.github.com>
2021-03-01 17:21:48 +0700

    Add files via upload

---
+++ Lab-01/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao \t cac ban!");
+		System.out.println("Hello \n World!");
+	}
+
+}
---
+++ Lab-01/LinearEquation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class LinearEquation {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input A: ");
+        double a = sc.nextDouble();
+        System.out.print("Input B: ");
+        double b = sc.nextDouble();
+        
+        if(a != 0) 
+        {
+            double c = -b/a;
+            System.out.printf("x = %f\n", c);
+        }
+        else 
+        {
+            System.out.printf("Error!!!\n");
+        }
+        sc.close();
+
+    }
+}
